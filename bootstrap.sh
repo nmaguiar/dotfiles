f119a70d9d53eb7812c0c9d526c78463c278df3e
Nuno Aguiar <nmaguiar@users.noreply.github.com>
2023-07-31 19:27:28 +0100

    Update bootstrap.sh adding ojob.io auto complete

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 curl https://openaf.io/nightly/install.sh | sh 
+echo if [ ! -e /etc/bash_completion.d/openaf-ojobio-complete ] || [ $(find /etc/bash_completion.d/openaf-ojobio-complete -mtime +1) ]; then ojob ojob.io/unix/ojobComplete > /etc/bash_completion.d/openaf-ojobio-complete; fi" >> /etc/bash.bashrc
