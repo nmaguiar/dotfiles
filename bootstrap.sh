1894e19a2e7f211203c39283d8c12e06bb81c1d7
Nuno Aguiar <nmaguiar@users.noreply.github.com>
2023-08-07 14:10:46 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,5 +4,5 @@ curl https://openaf.io/nightly/install.sh | sh
 echo if [ ! -e /etc/bash_completion.d/openaf-ojobio-complete ] || [ $(find /etc/bash_completion.d/openaf-ojobio-complete -mtime +1) ]; then ojob ojob.io/unix/ojobComplete > /etc/bash_completion.d/openaf-ojobio-complete; fi" >> /etc/bash.bashrc
 
 if [ "$GITHUB_REPOSITORY" = "nmaguiar/test" ]; then
-  . <(/usr/bin/bash /workspaces/$(echo $GITHUB_REPOSITORY | sed 's/nmaguiar\///'))/.github/bootstrap.sh
+  . <(/usr/bin/bash /workspaces/$(echo $GITHUB_REPOSITORY | sed 's/nmaguiar\///')/.github/bootstrap.sh)
 fi
