a4db0e88c75c65da70195f772a482712861387bf
Nuno Aguiar <nmaguiar@users.noreply.github.com>
2025-12-12 02:12:21 +0000

    Refactor bootstrap.sh to uncomment repository check and streamline environment export

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
-if [ "$GITHUB_REPOSITORY" = "nmaguiar/test" ]; then
-  /usr/bin/bash /workspaces/$(echo $GITHUB_REPOSITORY | sed 's/nmaguiar\///')/.github/bootstrap.sh > /tmp/bootstrap.log 2>&1 
-else
-  export > /tmp/bootstrap.env
-fi
+# if [ "$GITHUB_REPOSITORY" = "nmaguiar/test" ]; then
+#   /usr/bin/bash /workspaces/$(echo $GITHUB_REPOSITORY | sed 's/nmaguiar\///')/.github/bootstrap.sh > /tmp/bootstrap.log 2>&1 
+# else
+export > /tmp/bootstrap.env
+#fi
 
-curl https://openaf.io/nightly/install.sh | sh 
-sudo bash -c "echo 'if [ ! -e ~/.openaf-ojobio-complete ] || [ \$(find ~/.openaf-ojobio-complete -mtime +1) ]; then ojob ojob.io/unix/ojobComplete > \$HOME/.openaf-ojobio-complete; fi' >> /etc/bash.bashrc"
-sudo bash -c "echo 'source \$HOME/.openaf-ojobio-complete' >> /etc/bash.bashrc"
+#curl https://openaf.io/nightly/install.sh | sh 
+sudo bash -c "mkdir -p /opt/oaf && curl https://openaf.io/getStatic.sh | DIST=t8 sh -s && ojob ojob.io/oaf/symlinks"
+sudo bash -c "echo 'export PATH=/opt/oaf:\$PATH' >> /etc/bash.bashrc"
+sudo bash -c "echo 'source ~/.openaf_completion_bash.sh > /dev/null 2>&1' >> /etc/bash.bashrc"
+sudo bash -c "echo 'if [ ! -e ~/.openaf_completion_bash.sh ] || [ $(find ~/.openaf_completion_bash.sh -mtime +1) ]; then' >> /etc/bash.bashrc"
+sudo bash -c "echo '  /opt/oaf/oaf --bashcompletion all > ~/.openaf_completion_bash.sh & disown' >> /etc/bash.bashrc"
+sudo bash -c "echo 'fi' >> /etc/bash.bashrc"
